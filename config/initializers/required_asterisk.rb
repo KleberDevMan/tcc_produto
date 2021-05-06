68eef704adc32fea7a31d91412c7200ea834f3ad
Kleber Junio <kleber.chaves@bioritmo.com.br>
2021-05-05 23:21:10 -0300

    add required asterisk

--- config/initializers/required_asterisk.rb
+++ config/initializers/required_asterisk.rb
@@ -6,24 +6,25 @@ module ActionView
       alias actual_label label
 
       def label method, content_or_options = nil, options = nil, &block
-        if content_or_options && content_or_options.class == Hash
-          options = content_or_options
-        else
-          content = content_or_options
-        end
-        if object.class != Ransack::Search
-          unless object.nil?
+        begin
+          if content_or_options && content_or_options.class == Hash
+            options = content_or_options
+          else
+            content = content_or_options
+          end
+          if object.present? and object.class != Ransack::Search and object.class != OpenStruct
             if object.class.validators_on(method).map(&:class).include? ActiveRecord::Validations::PresenceValidator
               if options.class != Hash
                 options = {class: " required"}
-                # else
-                #     options[:class] = ((options[:class] || "") + " required")
-                #               .split.uniq.join Settings.salary_module.space
+              else
+                options[:class] = ((options[:class] || "") + " required").split.uniq
               end
             end
           end
+          actual_label(method, content, options || {}, &block)
+        rescue
         end
-        actual_label(method, content, options || {}, &block)
+
       end
     end
   end
